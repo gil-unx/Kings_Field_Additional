22c12425a673f9784a449913e377e5419280107a
gil-unx <57790408+gil-unx@users.noreply.github.com>
2023-12-19 12:10:29 +0700

    up

---
+++ Kings_Field_Additional/bin/Debug/repack.bat
@@ -0,0 +1,3 @@
+Kings_Field_Additional.exe -r main.dec 
+Kings_Field_Additional.exe -e main.dec 
+UMD-replace_CUE\UMD-replace.exe Kings_Field_Additional_I_JPN_PSP-DMU.iso PSP_GAME\USRDIR\GameData\01_UmdTitle\main.dat main.dat.new
---
+++ Kings_Field_Additional/bin/Debug/unpack.bat
@@ -0,0 +1,2 @@
+Kings_Field_Additional.exe -d main.dat 
+Kings_Field_Additional.exe -u main.dec 
